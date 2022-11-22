ce6a54ecf8ee575a58cd220044fec9d68a5c713f
YAZEED ALDAMEN <109088904+YazeedAldamen@users.noreply.github.com>
2022-11-23 00:20:08 +0300

    tasks

---
+++ 22-11 tasks/Optional task 8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers seperated by spaces: ");
+            string inputs = Console.ReadLine();
+            string[] numArray = inputs.Split(' ');
+            double sum = 0;
+            int entries = 0;
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                foreach (string o in numArray)
+                {
+                    if (o == numArray[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        sum += Convert.ToDouble(numArray[i]);
+                        entries++;
+                    }
+                }
+
+            }
+            double avg = sum / entries;
+            Console.WriteLine($"The average is: {avg}");
+
+        }
+    }
+}
